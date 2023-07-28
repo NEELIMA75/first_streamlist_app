5c4fb60f87b8656109e78518295cc013b870dd51
NEELIMA75 <140714056+NEELIMA75@users.noreply.github.com>
2023-07-29 01:30:19 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,1 @@
-create streamlit_app.py
-created the main python file
-import streamlit
-streamlit.title('my parents new healthy diner')
 
-
-import streamlit
-streamlit.title('my parents new healthy diner')
