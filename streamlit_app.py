7ccce3509d812b042e0a57adcde2828bd604e240
NEELIMA75 <140714056+NEELIMA75@users.noreply.github.com>
2023-08-01 00:51:33 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,6 +11,9 @@ streamlit.text('Hard-Boiled Free-Range Egg')
 streamlit.header('Breakfast favorites')
 
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
 
 
 
