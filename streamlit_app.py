aa15f8ea91b5615447937d3f8d96a78a83333a84
NEELIMA75 <140714056+NEELIMA75@users.noreply.github.com>
2023-08-13 00:33:31 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,6 +37,7 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
 import requests
 streamlit.dataframe(fruityvice_normalized)
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
 
 
 
