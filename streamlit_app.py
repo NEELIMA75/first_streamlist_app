ceedcac90a010c93ee701136dcb2fee7c685488f
NEELIMA75 <140714056+NEELIMA75@users.noreply.github.com>
2023-08-13 00:44:13 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,7 +36,7 @@ streamlit.header("Fruityvice Fruit Advice!")
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
 import requests
-streamlit.dataframe(fruityvice_normalized)
+
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
