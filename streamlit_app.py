43afcccc3ddeeafdf5f83d75bae2d7138c3603f3
NEELIMA75 <140714056+NEELIMA75@users.noreply.github.com>
2023-08-14 23:50:48 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,7 +43,7 @@ fruit_choice = streamlit.text_input('What fruit would you like information about
 streamlit.write('The user entered ', apple)
 import request
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "apple")
-
+import request
 
 
 
