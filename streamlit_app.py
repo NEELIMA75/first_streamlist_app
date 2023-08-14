3805d68bc71b480050012ab664ae07709e3fe6eb
NEELIMA75 <140714056+NEELIMA75@users.noreply.github.com>
2023-08-14 23:52:15 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,6 +45,8 @@ import request
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "apple")
 import request
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
+
 
 
 
